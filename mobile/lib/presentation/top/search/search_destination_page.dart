eba3d70a20b4c0878ac84b062695d737e852e0c7
Yoshiki Naruo <yoshiki20010524@icloud.com>
2022-10-15 23:02:39 +0900

    add: modal_search_sheet

---
+++ mobile/lib/presentation/top/search/search_destination_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SearchDestinationPage extends HookConsumerWidget {
+  const SearchDestinationPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold();
+  }
+}
--- mobile/lib/presentation/top/top_page.dart
+++ mobile/lib/presentation/top/top_page.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobile/l10n/app_localization.dart';
+import 'package:mobile/presentation/top/search/search_destination_page.dart';
 import 'package:mobile/presentation/top/top_controller_provider.dart';
 import 'package:mobile/presentation/top/top_state.dart';
 import 'package:mobile/presentation/widgets/search_button.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class TopPage extends StatefulHookConsumerWidget {
   const TopPage({
@@ -56,7 +58,9 @@ class TopPageState extends ConsumerState<TopPage> {
                     bottom: 70,
                     child: GlowingButton(
                       title: l10n.search_place_button,
-                      onTap: () {},
+                      onTap: () {
+                        _buildButtomSheet();
+                      },
                     ),
                   ),
                 ],
@@ -88,6 +92,14 @@ class TopPageState extends ConsumerState<TopPage> {
       onMapCreated: (GoogleMapController mapController) {
         _mapController.complete(mapController);
       },
+    );
+  }
+
+  void _buildButtomSheet() {
+    showCupertinoModalBottomSheet(
+      context: context,
+      expand: true,
+      builder: (_) => const SearchDestinationPage(),
     );
   }
 }
