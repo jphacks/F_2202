dcc94de80a85f57e85623153c744c26e9e33e428
Yoshiki Naruo <yoshiki20010524@icloud.com>
2022-10-15 22:53:13 +0900

    add: l10n

---
+++ mobile/lib/l10n/app_localization_ja.dart
@@ -0,0 +1,9 @@
+import 'app_localization.dart';
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationJa extends AppLocalization {
+  AppLocalizationJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get search_place_button => '場所を探す';
+}
