f3b2b59361921bc2901706b83d00508e94c57a4d
OkazakiRui <okazaki200226@gmail.com>
2022-10-19 04:39:55 +0900

    feat: Positionedで実装するように変更した

--- mobile/lib/presentation/widgets/header_back_button.dart
+++ mobile/lib/presentation/widgets/header_back_button.dart
@@ -7,21 +7,25 @@ class HeaderBackButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
+    // 呼び出し元に依存してしまうため，なるおニキに相談
+    // headerをstackで実装すると決まったらそのままにする
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + 8,
+      left: 16,
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+          color: Colors.white,
         ),
-        color: Colors.white,
-      ),
-      // TODO: 左右にmargin16pxを入れようとするとバグる
-      // margin: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: SizedBox(
-        width: 44,
-        height: 44,
-        child: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () => Navigator.of(context).pop(),
+        child: SizedBox(
+          width: 44,
+          height: 44,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
         ),
       ),
     );
