0803256b4cc200a7d137042d2f790a3572a62461
OkazakiRui <okazaki200226@gmail.com>
2022-10-19 04:40:25 +0900

    feat: headerとmainvisualのレイアウトを作成した

---
+++ mobile/lib/presentation/detail/detail_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/presentation/widgets/header_back_button.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: const Header(),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Column(
+            children: const [
+              MainVisual(),
+            ],
+          ),
+          const HeaderBackButton(),
+          const HeaderActionButtons(),
+        ],
+      ),
+    );
+  }
+}
+
+// 右側のボタン群
+class HeaderActionButtons extends StatelessWidget {
+  const HeaderActionButtons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + 8,
+      right: 16,
+      child: Row(
+        children: [
+          IconButton(
+            icon: const Icon(
+              Icons.share,
+              color: Colors.white,
+              size: 32,
+            ),
+            onPressed: () => {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.favorite_border,
+              color: Colors.white,
+              size: 32,
+            ),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// メインビジュアル
+class MainVisual extends StatelessWidget {
+  const MainVisual({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image(
+          image: const NetworkImage(
+              'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=800&q=80'),
+          fit: BoxFit.cover,
+          width: MediaQuery.of(context).size.width,
+          height: 270,
+        ),
+        const ImageGradationOverlay(),
+      ],
+    );
+  }
+}
+
+// メインビジュアルのグラデーション
+class ImageGradationOverlay extends StatelessWidget {
+  const ImageGradationOverlay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      left: 0,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 270,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0x00000000), Colors.black],
+          ),
+        ),
+      ),
+    );
+  }
+}
