a6e314c53b8681f125d89fc562a9115f018a1ceb
mi11km <mi11km.program@gmail.com>
2022-10-22 01:26:09 +0900

    refactor

--- backend/api/api/interfaces/http/router.py
+++ backend/api/api/interfaces/http/router.py
@@ -1,16 +1,21 @@
 import os
-from http import HTTPStatus
 
 import requests
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-from gensim.models import KeyedVectors
 
 from api.interfaces.http.dto import GoogleMapNearbySearchResponse, Health
 
+# from http import HTTPStatus
+
+
+# from fastapi.responses import JSONResponse
+# from gensim.models import KeyedVectors
+
+
 router = APIRouter()
 
-model = KeyedVectors.load_word2vec_format("./jawiki.word_vectors.100d.txt")
+
+# model = KeyedVectors.load_word2vec_format("./jawiki.word_vectors.100d.txt")
 
 
 @router.get("/health", response_model=Health)
@@ -18,16 +23,16 @@ async def health() -> Health:
     return Health(health="ok")
 
 
-@router.get("/recommend/keywords", status_code=200)
-async def recommend_keywords(search_word: str):
-    try:
-        candidate_words = model.most_similar(search_word)
-        return {"candidate_words": candidate_words}
-    except Exception:
-        return JSONResponse(
-            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            content={"candidate_words": []},
-        )
+# @router.get("/recommend/keywords", status_code=200)
+# async def recommend_keywords(search_word: str):
+#     try:
+#         candidate_words = model.most_similar(search_word)
+#         return {"candidate_words": candidate_words}
+#     except Exception:
+#         return JSONResponse(
+#             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+#             content={"candidate_words": []},
+#         )
 
 
 @router.get("/location/nearby")
@@ -39,14 +44,15 @@ async def fetch_nearby_location_from_keyword(
     )
     place_endpoint = "https://maps.googleapis.com/maps/api/place/details/json"
     api_key = os.getenv("GOOGLE_MAP_API_KEY")
-    urls = [
-        f"{nearby_location_endpoint}?key={api_key}&location={lat}%2C{lon}&radius={radius}&language=ja&keyword={key}"
-        for key in keywords.split(",")
-    ]
-    locations: list[GoogleMapNearbySearchResponse] = [
-        GoogleMapNearbySearchResponse(**requests.get(url).json()["results"])
-        for url in urls
-    ]  # 並列化したい
+    keys = keywords.split(",")
+    locations: list[GoogleMapNearbySearchResponse] = []
+    for key in keys:
+        locations += [
+            GoogleMapNearbySearchResponse(**loc)
+            for loc in requests.get(
+                f"{nearby_location_endpoint}?key={api_key}&location={lat}%2C{lon}&radius={radius}&language=ja&keyword={key}"
+            ).json()["results"]
+        ]  # 並列化したい
     locations.sort(key=lambda location: location.rating, reverse=True)
     locations = locations if len(locations) < count else locations[:count]
     return [
